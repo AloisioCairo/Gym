0dafe6873ee76d04f7cf62bcd2ca5e02dcae01ee
aloisiocairo <aloisiocairo@gmail.com>
2020-07-02 08:39:37 -0300

    Implementações referente ao cadastro de instrutores, salvando os dados no arquivo "data.json"

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const routes = require('./routes')
 
 const server = express()
 
+/* Cadastro de Instrutores - Recebendo dados do Front end com req.body
+   Configura o Express para receber os dados do "body" */
+server.use(express.urlencoded({extended: true}))
+
 server.use(express.static('public'))
 server.use(routes)
 
