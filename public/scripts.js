13044484c8014b4c167889483b26a2cdddbd455e
aloisiocairo <aloisiocairo@gmail.com>
2020-07-04 16:18:14 -0300

    Implementações diversas para o cadastro de intrutores e membros

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+/* Membros - Lógica do menu ativo */
+const currentPage = location.pathname
+const menuItens = document.querySelectorAll("header .links a")
+
+for (item of menuItens) {
+    if (currentPage.includes(item.getAttribute("href"))) {
+        item.classList.add('active')
+    }
+}
